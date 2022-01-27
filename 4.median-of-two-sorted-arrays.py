31334c552ec15717f9136b3eafa9d49189566216
AllenJShi <allenjunsheng@gmail.com>
2022-01-26 22:10:23 -0500

    01262022

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -12,19 +12,47 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        ################### 我的答案 ################
+        ######### 直接sorted():
+        # m, n = len(nums1), len(nums2)
+        # odd = (m+n) % 2
+        # nums = sorted(nums1 + nums2)
+        # if odd:
+        #     median_pos = (m+n) // 2
+        #     # go and find the median_pos th element in the sorted combined array
+        #     return nums[median_pos]
+        # else:
+        #     median_left = (m+n) / 2 - 1
+        #     median_right = (m+n) / 2 
+        #     return (nums[median_left]+nums[median_right]) / 2.0
+        
+        ######### 思考：
         m, n = len(nums1), len(nums2)
-        odd = (m+n) % 2
-        p1,p2 = 0, 0
-        ans = 0
-        nums = sorted(nums1 + nums2)
-        if odd:
-            median_pos = (m+n) // 2
-            # go and find the median_pos th element in the sorted combined array
-            
-            return nums[median_pos]
+        def getKthElement(k):
+            index1, index2 = 0, 0 
+            while True:
+                if index1 == m:
+                    return nums2[index2 + k -1]
+                if index2 == n:
+                    return nums1[index1 + k -1]
+                if k == 1:
+                    return min(nums1[index1],nums2[index2])
+                newIndex1 = min(index1+k//2-1,m-1)
+                newIndex2 = min(index2+k//2-1,n-1)
+                pivot1, pivot2 = nums1[newIndex1], nums2[newIndex2]
+                if pivot1 <= pivot2:
+                    k -= newIndex1 - index1 + 1
+                    index1 = newIndex1 + 1
+                else:
+                    k -= newIndex2 - index2 + 1
+                    index2 = newIndex2 + 1
+        totalLength = m+n
+        if totalLength%2 == 1:
+            return getKthElement((totalLength+1)//2)
         else:
-            median_left = (m+n) / 2 -1
-            median_right = (m+n) / 2 
-            return (nums[median_left]+nums[median_right]) / 2.0
+            return (getKthElement(totalLength//2)+getKthElement(totalLength//2+1))/2.0
+        
+            
+        
 # @lc code=end
 
---
+++ 437.path-sum-iii.py
@@ -0,0 +1,64 @@
+#
+# @lc app=leetcode id=437 lang=python
+#
+# [437] Path Sum III
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def pathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: int
+        """
+        ############# DFS ###################
+        # def backtracking(root,targetSum):
+        #     if not root: return 0 
+        #     res = 0        
+        #     if root.val == targetSum:
+        #         res += 1
+        #     res += backtracking(root.left, targetSum - root.val)
+        #     res += backtracking(root.right, targetSum - root.val)
+        #     return res
+        # if not root: return 0
+        # res = backtracking(root,targetSum)
+        # res += self.pathSum(root.left, targetSum)
+        # res += self.pathSum(root.right, targetSum)
+        # return res
+    
+        # time O(N^2) | space O(N)
+        
+        ############### 前缀和 ####################
+        #######思路：当前元素的路径之和
+        # 记录从root到当前结点除当前节点外的前缀和
+        prefixSum = collections.defaultdict(int)
+        prefixSum[0] = 1
+        
+        def backtracking(root,curr):
+            # 终止条件
+            if not root: return 0
+            res = 0
+            # 前缀和加上当前结点的val
+            curr += root.val
+            # 从之前的前缀和中寻找
+            res += prefixSum[curr-targetSum]
+            prefixSum[curr] += 1
+            res += backtracking(root.left,curr)
+            res += backtracking(root.right,curr)
+            # 回溯
+            prefixSum[curr] -= 1
+            return res
+        return backtracking(root,0)
+        
+        # time O(n) | space O(n)
+        
+        
+# @lc code=end
+
