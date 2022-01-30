c48d0ef1b20d9f4cb3c5f0a246398fd359d5d8f8
AllenJShi <allenjunsheng@gmail.com>
2022-01-30 12:47:58 -0500

    01302022

--- 300.longest-increasing-subsequence.py
+++ 300.longest-increasing-subsequence.py
@@ -11,19 +11,44 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        dp = [0] * (n)
-        dp[0] = 1
-        maxL = 1
-        for i in range(1,n):
-            # i本身就是一个单位的sub
-            dp[i] = 1
-            # 从0到i-1
-            for j in range(0,i):
-                # 如果对于每一个i之前的j，存在i大于j的情况
-                if nums[i] > nums[j]:
-                    # dp更新为当前的dpi或dpj加上一个1中的max
-                    dp[i] = max(dp[i],dp[j]+1)
-        return max(dp)
+        ########### DP ###############
+        # n = len(nums)
+        # dp = [0] * (n)
+        # dp[0] = 1
+        # for i in range(1,n):
+        #     # i本身就是一个单位的sub
+        #     dp[i] = 1
+        #     # 从0到i-1
+        #     for j in range(0,i):
+        #         # 如果对于每一个i之前的j，存在i大于j的情况
+        #         if nums[i] > nums[j]:
+        #             # dp更新为当前的dpi或dpj加上一个1中的max
+        #             dp[i] = max(dp[i],dp[j]+1)
+        # return max(dp)
+        
+        ########### Greedy + Binary Search ################
+        ####### 思路：希望上升的慢，这样总共长度不变，子长度可能会更长
+        # 维护一个数组 d[i] 记录长度为i的末尾最小值
+        # d是单调递增的
+        d = []
+        for n in nums:
+            # 若当前num比之前最长长度的末尾元素大，当前num可以被加在之前的长度后
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                # 否则，向前寻找第一个比当前num小的末尾元素
+                # 二分查找d，因为d代表每个长度的最末尾元素
+                # 如果找到那个仅次于当前num的末尾元素，那么当前元素将是d中r的下一个
+                l, r = 0, len(d) - 1
+                loc = l
+                while l <= r:
+                    mid = (l+r) // 2
+                    if d[mid] >= n:
+                        loc = mid
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                d[loc] = n
+        return len(d)
 # @lc code=end
 
---
+++ 394.decode-string.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=394 lang=python
+#
+# [394] Decode String
+#
+
+# @lc code=start
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ################ 递归 ################
+        def backtracking(i):
+            res, multi = "", 0
+            while i < len(s):
+                if '0' <= s[i] <= '9':
+                    multi = multi * 10 + int(s[i])
+                elif s[i] == '[':
+                    i, tmp = backtracking(i+1)
+                    res += tmp * multi
+                    multi = 0
+                elif s[i] == ']':
+                    return i, res
+                else:
+                    res += s[i]
+                i += 1
+            return res
+        return backtracking(0)
+        
+        ############## 迭代 ###############
+        stack, res, multi = [], "", 0
+        for c in s:
+            if c == '[':
+                stack.append([multi,res])
+                res, multi = "",0
+            elif c == ']':
+                cur_multi, last_res = stack.pop()
+                res = last_res + cur_multi * res
+            elif '0' <= c <= '9':
+                multi = multi * 10 + int(c)
+            else:
+                res += c
+        return res
+# @lc code=end
+
+
